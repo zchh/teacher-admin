ed6e521e5268d43af9cad658fa32cd70666a708a
RagPanda <RagPanda@jtcool.com>
2015-10-19 17:19:12 +0800

    测试提交 2015/10/19

--- gird_plugins/Base/class/PowerFunc.php
+++ gird_plugins/Base/class/PowerFunc.php
@@ -3,15 +3,35 @@ namespace GirdPlugins\Base;
 use \Illuminate\Support\Facades\Session;
 class PowerFunc
 {
-    
+    /**
+     * 管理员权限检查
+     * 
+     * 
+     * @access public
+     * @param int $powerId
+     * 
+     *
+     * @return Bool
+     */
     public function checkAdminPower($powerId)
     {
         
     }
+    /**
+     * 用户权限检查
+     * 
+     * 
+     * @access public
+     * @param int $powerId
+     * 
+     *
+     * @return Bool
+     */
      public function checkUserPower($powerId)
     {
         
     }
+
     public function addAdminPowerGroup()
     {
         
