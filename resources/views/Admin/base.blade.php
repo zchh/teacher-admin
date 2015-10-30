3dfb08414f10281a15b2ae5a2af4c331888b0a0d
RagPanda <RagPanda@jtcool.com>
2015-10-30 21:29:55 +0800

    准备和王宇飞合并 15/10/30 21:29
    Conflicts:
    	app/Http/routes.php
    	nbproject/private/private.xml
    	resources/views/Admin/Power/base.blade.php
    	resources/views/Admin/base.blade.php

--- resources/views/Admin/base.blade.php
+++ resources/views/Admin/base.blade.php
@@ -20,6 +20,7 @@
                   <a class="navbar-brand" href="#">Base CMS</a>
                 </div>
 
+                
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                   <ul class="nav navbar-nav">
