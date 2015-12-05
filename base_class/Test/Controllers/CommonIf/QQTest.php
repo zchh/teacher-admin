418dfac8427f8b878be30732e787b1a63ddede12
pengliang <1174495058@qq.com>
2015-12-05 19:51:20 +0800

    -2  12/5 19:51

--- base_class/Test/Controllers/CommonIf/QQTest.php
+++ base_class/Test/Controllers/CommonIf/QQTest.php
@@ -32,8 +32,7 @@ class QQTest extends Controller
             if($data != false)
             {
                 //把返回来的token和openID存入数据库
-                $input_data = DB::table("base_token")->where("access_token","=",$data['access_token'])
-                    ->where("openID","=",$data['openID'])->first();
+                $input_data = DB::table("base_token")->where("openID","=",$data['openID'])->first();
                 if(empty($input_data))
                 {
                     $base_data['access_token'] = $data['access_token'];
@@ -43,7 +42,7 @@ class QQTest extends Controller
                 }
                 $user_data = User::qqLogin($data['access_token'], $data['openID']);
                 //dump($user_data);
-                if ($user_data === true)
+                if ($user_data !== false && $user_data !== true)
                 {
                     //echo '登录成功,系统已经为你自动注册';
                     //dump($user_data);
@@ -51,7 +50,11 @@ class QQTest extends Controller
                     $baseFunc->setRedirectMessage(true,"系统已自动为你注册，为了你的个人信息安全请及时修改密码和个人资料！<a href='#'>www.baidu.com</a>",null,"/user_index");
                     //echo "<script language='javascript'>alert('系统已自动为你注册，为了你的个人信息安全请及时修改密码和个人资料!');window.location.href='/user_index';</script>";
                 }
-
+                else
+                {
+                    $baseFunc = new BaseFunc();
+                    $baseFunc->setRedirectMessage(true,"登录成功",null,"/user_index");
+                }
             }
         }
     }
