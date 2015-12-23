98b6c04510b0c19e5fa673c701e323daf5b14358
zhangchi <zhangchi@local.com>
2015-12-23 12:02:00 +0800

    12/23 zc 1

--- config/my_config.php
+++ config/my_config.php
@@ -4,7 +4,7 @@ return [
     "title_name" => "Base CMS",
     
     "mail_head_name" => "Base CMS",
-    "website_url"=>"http://127.0.0.6",          //网站url
+    "website_url"=>"http://127.0.0.5:8080",          //网站url
     "secure_check_url" => "/user_checkMailUrl",//安全检查跳转回的url
     "image_upload_dir" => "/../storage/app/image/",//文件上载路径
     "special_page" => ["/putImage?"],    //可以不检查token的页面
